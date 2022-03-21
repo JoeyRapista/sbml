0d4c62e8a79e72b8cda55e1d8e7a0afcf5cd9c7a
joey <yeojrapista@gmail.com>
2022-03-21 14:07:07 +0800

    added new database uri

--- sbml/__init__.py
+++ sbml/__init__.py
@@ -6,7 +6,7 @@ from flask_ckeditor import CKEditor
 app = Flask(__name__)
 ckeditor = CKEditor(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://leswfjzczrpnnk:37b638d0c546f0a0f392a6917eaaf4c4b553121c6e32c74f1215e4f6a4c9af5a@ec2-3-222-204-187.compute-1.amazonaws.com:5432/d4ta0uv18bmlse'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cgiowbcdqoyezw:16b9052877e5b125b0864764992b150e8aa75efcc58883f54de97897776187d9@ec2-3-231-254-204.compute-1.amazonaws.com:5432/d4e871oljt4tb5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "as#asdASASF#ASFLKsndvssdf2p2990$"
 
