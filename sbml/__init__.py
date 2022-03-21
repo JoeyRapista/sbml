8b4ea5f5690d7c248f310f24aeca2b93bfe351f9
joey <yeojrapista@gmail.com>
2022-03-21 13:13:58 +0800

    tweaked for heroku

--- sbml/__init__.py
+++ sbml/__init__.py
@@ -5,7 +5,8 @@ from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 ckeditor = CKEditor(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://leswfjzczrpnnk:37b638d0c546f0a0f392a6917eaaf4c4b553121c6e32c74f1215e4f6a4c9af5a@ec2-3-222-204-187.compute-1.amazonaws.com:5432/d4ta0uv18bmlse'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "as#asdASASF#ASFLKsndvssdf2p2990$"
 
