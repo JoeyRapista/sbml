7fd483b76304f09ef2a1cf3096997f0fc56a3c60
joey <yeojrapista@gmail.com>
2022-03-24 10:33:36 +0800

    changed db

--- sbml/__init__.py
+++ sbml/__init__.py
@@ -5,8 +5,8 @@ from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 ckeditor = CKEditor(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://frfkmnxwpuvuky:c256aa296020c2d5e27af56d89e6f22fe00a9d256e448832e31045b1739a2f75@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d1jbbtsblqp0rc'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://frfkmnxwpuvuky:c256aa296020c2d5e27af56d89e6f22fe00a9d256e448832e31045b1739a2f75@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d1jbbtsblqp0rc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "as#asdASASF#ASFLKsndvssdf2p2990$"
 
